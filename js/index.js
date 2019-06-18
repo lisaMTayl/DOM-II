6551b8408f9158981d66bf7918df6d87e55c8f71
Lisa <lmkirk@gmail.com>
2019-06-18 19:09:47 -0400

    lisa-taylor

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// nav
+
 const navHover = document.querySelector('nav');
 navHover.addEventListener('mouseover', event => {
     event.target.style.color = "purple";
@@ -19,35 +19,36 @@ navBorder.addEventListener('click', event => {
     event.target.style.border = "";
 });
 
-
-// right click
 const purpleTxt = document.querySelector('p');
 purpleTxt.addEventListener('contextmenu', (event) => {
     event.target.style.backgroundColor = 'purple';
 });
 
-
-// double click
 const orangeTxt = document.querySelector('h2');
 orangeTxt.addEventListener('dblclick', event => {
    event.target.style.color = "orange";
 });
 
-// select
-const footerP = document.querySelector('footer p');
-footerP.addEventListener('select', event => {
-    event.target.style.fontStyle = "bold";
-    event.preventDefault();
-});
 footerP.addEventListener('copy', event => {
     event.clipboardData.setData('text/plain',"Secret words!");
     event.preventDefault();
 });
+
 footerP.addEventListener('cut', event => {
     event.clipboardData.setData('text/plain', "Still secret!");
     event.preventDefault();
 });
 
+// someday we'll have an input section
+const inputTxt = document.querySelector('input');
+inputTxt.addEventListener('select', event => {
+    event.target.style.fontStyle = "bold";
+    event.preventDefault();
+});
+
+inputTxt.addEventListener('focus', event => {
+    event.target.style.backgroundColor = "pink";
+});
 
 
 

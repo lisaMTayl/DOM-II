00358d0481083bd4f9b3e5f3a2bc868cfb525ed0
Lisa <lmkirk@gmail.com>
2019-06-18 17:32:51 -0400

    bold footer

--- js/index.js
+++ js/index.js
@@ -14,10 +14,18 @@ purpleTxt.addEventListener('contextmenu', (event) => {
     event.target.style.backgroundColor = 'purple';
 });
 
+// double click
 const orangeTxt = document.querySelector('h2');
 orangeTxt.addEventListener('dblclick', event => {
    event.target.style.color = "orange";
 });
+
+const footBold = document.querySelector('footer');
+footBold.addEventListener('select', event => {
+    event.target.style.fontStyle = "bold";
+});
+
+
 
 
 

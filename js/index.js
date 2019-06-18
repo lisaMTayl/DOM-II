6bccb73d2674e7322a1426923b863d876e234169
Lisa <lmkirk@gmail.com>
2019-06-18 15:04:20 -0400

    nav hover purple

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const navHover = document.querySelector('nav');
+navHover.addEventListener('mouseover', event => {
+    event.target.style.color = "purple";});
+navHover.addEventListener('mouseout', event => {
+    event.target.style.color = "";
+});
+
+

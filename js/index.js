00532c1969c99beb74921d20d2cd3a32b9daa0f0
Lisa <lmkirk@gmail.com>
2019-06-18 15:35:29 -0400

    readme

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 // Your code goes here
 
+// mouseover, mouseout
 const navHover = document.querySelector('nav');
 navHover.addEventListener('mouseover', event => {
     event.target.style.color = "purple";});
 navHover.addEventListener('mouseout', event => {
     event.target.style.color = "";
+});
+
+// keydown
+const pressKey = document.querySelector('logo-heading');
+pressKey.addEventListener('keypress', event => {
+   
 });
 
 

2952f755999538defa16919415a979408fe18317
Lisa <lmkirk@gmail.com>
2019-06-18 18:26:00 -0400

    nav border

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // Your code goes here
 
-// mouseover, mouseout
+// nav
 const navHover = document.querySelector('nav');
 navHover.addEventListener('mouseover', event => {
     event.target.style.color = "purple";});
+
 navHover.addEventListener('mouseout', event => {
     event.target.style.color = "";
 });
+
+const navBorder = document.querySelector('header');
+navBorder.addEventListener('dblclick', event => {
+    event.target.style.border = "5px dashed red";
+});
+navBorder.addEventListener('click', event => {
+    event.target.style.border = "";
+});
+
 
 // right click
 const purpleTxt = document.querySelector('p');
 purpleTxt.addEventListener('contextmenu', (event) => {
     event.target.style.backgroundColor = 'purple';
 });
+
 
 // double click
 const orangeTxt = document.querySelector('h2');
@@ -30,6 +41,7 @@ footerP.addEventListener('copy', event => {
     event.clipboardData.setData('text/plain',"Secret words!");
     event.preventDefault();
 });
+
 
 
 
